b8970c4462a4c9fb4168f7f75bc14fd6c7aa218f
Chang Doe <kontsuyo@users.noreply.github.com>
2025-03-06 07:49:00 +0900

    fix:#19 テストを修正

--- djangoapp/notes/tests/test_models.py
+++ djangoapp/notes/tests/test_models.py
@@ -1,11 +1,16 @@
+from django.contrib.auth import get_user_model
 import pytest
 
 from notes.models import Whiskey
+
+User = get_user_model()
 
 
 @pytest.mark.django_db
 class TestWhiskeyModel:
     def test_register_whiskey(self):
+        user = User.objects.create_user(username="whiskey-lover")
+
         name = "タリスカー"
         country = "SC"
         alcohol = 45.8
@@ -18,5 +23,6 @@ class TestWhiskeyModel:
             alcohol=alcohol,
             cask=cask,
             price=price,
+            owner=user,
         )
         assert Whiskey.objects.filter(id=talisker.id).exists()  # type: ignore
