a147107c652e86c2c2caf8dfbfa7246f254c95e2
Chang Doe <kontsuyo@users.noreply.github.com>
2025-03-05 15:30:28 +0900

    feat: #11 TokenAuthentication

--- djangoapp/config/settings.py
+++ djangoapp/config/settings.py
@@ -41,9 +41,16 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "rest_framework.authtoken",
     "accounts",
     "notes",
 ]
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.TokenAuthentication",
+    ]
+}
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
