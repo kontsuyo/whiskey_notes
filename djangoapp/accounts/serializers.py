1fb7276ecf302d98ab6c5a6b8d0ef574f6c0ff6b
Chang Doe <kontsuyo@users.noreply.github.com>
2025-04-06 15:31:35 +0900

    userserializerをいじったよ

--- djangoapp/accounts/serializers.py
+++ djangoapp/accounts/serializers.py
@@ -1,22 +1,21 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
-from .models import TastingNoteUser
-
-UserModel = get_user_model()
+User = get_user_model()
 
 
-class CustomUserSerializer(serializers.ModelSerializer):
+class TastingNoteUserSerializer(serializers.ModelSerializer):
 
     password = serializers.CharField(write_only=True)
 
     def create(self, validated_data):
-        user = UserModel.objects.create_user(
+        user = User.objects.create_user(
             username=validated_data["username"],
             password=validated_data["password"],
+            email=validated_data["email"],
         )
         return user
 
     class Meta:
-        model = TastingNoteUser
+        model = User
         fields = ["id", "username", "password", "email"]
--- djangoapp/accounts/tests/test_serializers.py
+++ djangoapp/accounts/tests/test_serializers.py
@@ -1,6 +1,29 @@
-# import pytest
-# from django.contrib.auth import get_user_model
+import logging
 
-# from accounts.serializers import CustomUserSerializer
+import pytest
+from django.contrib.auth import get_user_model
 
-# UserModel = get_user_model()
+from accounts.serializers import TastingNoteUserSerializer
+
+UserModel = get_user_model()
+logger = logging.getLogger(__name__)
+
+
+@pytest.mark.django_db
+def test_validate_data_at_create_user(user_payload):
+    serializer = TastingNoteUserSerializer(data=user_payload)
+    assert serializer.is_valid()
+    logger.info(f"serializer.data:           {serializer.data}")
+    logger.info(f"serializer.validated_data: {serializer.validated_data}")
+    user_create = serializer.create(validated_data=serializer.validated_data)
+    assert user_create.username == user_payload["username"]
+    assert user_create.check_password(user_payload["password"])
+    assert user_create.email == user_payload["email"]
+
+
+@pytest.mark.django_db
+def test_password_is_write_only_field(user_payload):
+    serializer = TastingNoteUserSerializer(data=user_payload)
+    assert serializer.is_valid()
+    logger.info(serializer.data)
+    assert "password" not in serializer.data
