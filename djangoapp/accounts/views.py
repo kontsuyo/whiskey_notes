cca080e6450acaea3abeb6d69c621e89bc86e9e6
Chang Doe <kontsuyo@users.noreply.github.com>
2025-04-09 12:30:13 +0900

    feat:#47 ログイン機能

--- djangoapp/accounts/views.py
+++ djangoapp/accounts/views.py
@@ -61,3 +61,19 @@ class RegisterView(APIView):
                     status=status.HTTP_400_BAD_REQUEST,
                 )
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+from rest_framework.authtoken.models import Token
+from rest_framework.authtoken.views import ObtainAuthToken
+
+
+class LoginView(ObtainAuthToken):
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(
+            data=request.data, context={"request": request}
+        )
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data["user"]  # type: ignore
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({"token": token.key, "user_id": user.pk, "email": user.email})
