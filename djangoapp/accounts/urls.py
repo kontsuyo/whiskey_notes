3050a40236df9a1a01e77f7d43abe1995e2ae490
Chang Doe <kontsuyo@users.noreply.github.com>
2025-04-15 17:58:03 +0900

    feat/#69 change url

--- djangoapp/accounts/urls.py
+++ djangoapp/accounts/urls.py
@@ -4,7 +4,7 @@ from accounts import views
 
 urlpatterns = [
     path("", views.UserList.as_view(), name="user-list"),
-    path("detail/<str:pk>", views.UserDetail.as_view(), name="user-detail"),
+    path("detail/<str:username>", views.UserDetail.as_view(), name="user-detail"),
     path("register/", views.RegisterView.as_view(), name="register-user"),
     path("login/", views.LoginView.as_view(), name="login-user"),
     path("update/<str:username>", views.UserUpdateView.as_view(), name="user-update"),
